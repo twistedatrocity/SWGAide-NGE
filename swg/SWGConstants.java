4cd73ff0597a5da8eded006c0b05dc974bdc5077
twistedatrocity <solarisjedi@gmail.com>
2019-06-27 01:03:13 -0500

    version

--- swg/SWGConstants.java
+++ swg/SWGConstants.java
@@ -74,7 +74,7 @@ public class SWGConstants {
     /**
      * The version number of the SWGAide that is available for download.
      */
-    public static final String version = "0.9.9-MrMiagi-0.1.20";
+    public static final String version = "0.9.9-MrMiagi-0.1.21";
 
     /**
      * Returns the cache directory.
