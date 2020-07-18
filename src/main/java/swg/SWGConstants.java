3f981136e7b6fa0d34f83f9eae71804636990d48
twistedatrocity <solarisjedi@gmail.com>
2020-07-18 15:24:59 -0500

    new version

--- src/main/java/swg/SWGConstants.java
+++ src/main/java/swg/SWGConstants.java
@@ -74,7 +74,7 @@ public class SWGConstants {
     /**
      * The version number of the SWGAide that is available for download.
      */
-    public static final String version = "Unity-1.1.2";
+    public static final String version = "Unity-1.1.3";
 
     /**
      * Returns the cache directory.
